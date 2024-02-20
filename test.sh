d209316ed22128e87e83cae7b90450bb222c7f18
Mgldvd <mgldvd@protonmail.com>
2024-02-19 20:58:46 -0500

    delete file test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-
-file="compose.yml"
-file_content=$(<"$file")
-ports=$(awk '/ports:/{getline; print $2}' <<< "$file_content")
-IFS=: read -r port1 port2 <<< "$ports" || { echo "Error: Invalid port format"; exit 1; }
-echo
-echo "  URL: http://localhost:$port1"
